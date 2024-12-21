e4456c58bfaa1cc4376037dd6d28ddbcebdb4bbd
udaychugh <udaychugh8@gmail.com>
2024-12-21 13:34:25 +0530

    dialog ui fix and minor chnages

--- app/src/main/java/com/freelab/tech/shootdown/game/hint/HintDialogFragment.kt
+++ app/src/main/java/com/freelab/tech/shootdown/game/hint/HintDialogFragment.kt
@@ -1,10 +1,16 @@
 package com.freelab.tech.shootdown.game.hint
 
+import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
+import com.freelab.tech.shootdown.R
 import com.freelab.tech.shootdown.databinding.DialogHintBinding
 
 class HintDialogFragment(
@@ -40,19 +46,30 @@ class HintDialogFragment(
         }
     }
 
+    override fun getTheme(): Int {
+        return R.style.FullScreenDialog
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = DialogHintBinding.inflate(inflater, container, false)
-
         setupInfo()
         setupClickListener()
-
+        isCancelable = false
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.decorView?.windowInsetsController?.apply {
+            hide(WindowInsets.Type.navigationBars())
+            systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
